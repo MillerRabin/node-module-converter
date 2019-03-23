16666e7d58e1aa5b76ac51b9e5ec4ac2195f9cc7
MillerRabin <millerrabin@raintech.su>
2019-03-24 00:12:18 +0300

    added comments

--- replace.js
+++ replace.js
@@ -1,6 +1,6 @@
 exports.toImport = function(str) {
     let output = str.replace(/module.exports\s*=\s*/, 'export default ');
-    output = output.replace(/const\s+(.+)\s*=\s*require\('(\.\/.+)'\);/g, 'import $1 from "$2";');
-    output = output.replace(/const\s+(.+)\s*=\s*require\('(.+)'\);/g, 'import $1 from "/node_modules/$2/browser/main.js";');
+    output = output.replace(/const\s+(.+)\s*=\s*require\('(\.\/.+)'\);/g, 'import $1 from "$2";');                              //const test = require('./test.js');
+    output = output.replace(/const\s+(.+)\s*=\s*require\('(.+)'\);/g, 'import $1 from "/node_modules/$2/browser/main.js";');    //const test = require('test.js');
     return output;
 };
