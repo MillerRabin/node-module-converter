8e0e38f75358848e8e64ecf22e54f461b71e34c1
MillerRabin <millerrabin@raintech.su>
2019-03-24 00:35:51 +0300

    added comments

--- replace.js
+++ replace.js
@@ -1,6 +1,7 @@
 exports.toImport = function(str) {
     let output = str.replace(/module.exports\s*=\s*/, 'export default ');
     output = output.replace(/const\s+(.+)\s*=\s*require\('(\.\/.+)'\);/g, 'import $1 from "$2";');                              //const test = require('./test.js');
-    output = output.replace(/const\s+(.+)\s*=\s*require\('(.+)'\);/g, 'import $1 from "/node_modules/$2/browser/main.js";');    //const test = require('test.js');
+    output = output.replace(/const\s+(.+)\s*=\s*require\('(.+)'\); \/\/Delete for browser environment/gi, '');                   //const test = require('test'); //Deleted for browser environment
+    output = output.replace(/const\s+(.+)\s*=\s*require\('(.+)'\);/g, 'import $1 from "/node_modules/$2/browser/main.js";');    //const test = require('test');
     return output;
 };
--- test/replace.js
+++ test/replace.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 
 const nodeModule = "const IntentionStorage = require('intention-storage');";
 const localModule = "const IntentionStorage = require('./main.js');";
+const deleteModule = "const delReq = require('del-req'); //Delete for Browser environment";
 
 describe('Intenion builder', function () {
     describe('toImport', function () {
@@ -14,5 +15,11 @@ describe('Intenion builder', function () {
             const nm = replace.toImport(localModule);
             assert.strictEqual(nm, 'import IntentionStorage  from "./main.js";');
         });
+
+        it('Delete require by comment', function () {
+            const nm = replace.toImport(deleteModule);
+            assert.strictEqual(nm, '');
+        });
+
     });
 });
